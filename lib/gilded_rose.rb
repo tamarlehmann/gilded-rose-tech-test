8a4ace1eb15d42ae67ec820146fc5249317e33e7
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-07 17:32:49 +0000

    Refactor change_quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,8 +1,8 @@
 require_relative 'item'
 
 class GildedRose
-  MIN_QUALITY = 0
-  MAX_QUALITY = 50
+  MIN_THRESHOLD = 0
+  MAX_THRESHOLD = 50
   BSPASS_TEN_DAY_THRESHOLD = 11
   BSPASS_FIVE_DAY_THRESHOLD = 6
 
@@ -13,23 +13,23 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if !is_aged_brie?(item) and !is_backstage_pass?(item)
-        if item.quality > MIN_QUALITY
+        if item.quality > MIN_THRESHOLD
           if !is_sulfuras?(item)
-            item.quality = item.quality - 1
+            change_quality(item, -1)
           end
         end
       else
-        if item.quality < MAX_QUALITY
+        if item.quality < MAX_THRESHOLD
           item.quality = item.quality + 1
           if is_backstage_pass?(item)
             if item.sell_in < BSPASS_TEN_DAY_THRESHOLD
-              if item.quality < MAX_QUALITY
-                item.quality = item.quality + 1
+              if item.quality < MAX_THRESHOLD
+                change_quality(item, +1)
               end
             end
             if item.sell_in < BSPASS_FIVE_DAY_THRESHOLD
-              if item.quality < MAX_QUALITY
-                item.quality = item.quality + 1
+              if item.quality < MAX_THRESHOLD
+                change_quality(item, +1)
               end
             end
           end
@@ -38,20 +38,20 @@ class GildedRose
       if !is_sulfuras?(item)
         item.sell_in = item.sell_in - 1
       end
-      if item.sell_in < 0
+      if item.sell_in < MIN_THRESHOLD
         if !is_aged_brie?(item)
           if !is_backstage_pass?(item)
-            if item.quality > MIN_QUALITY
+            if item.quality > MIN_THRESHOLD
               if !is_sulfuras?(item)
-                item.quality = item.quality - 1
+                change_quality(item, -1)
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
-          if item.quality < MAX_QUALITY
-            item.quality = item.quality + 1
+          if item.quality < MAX_THRESHOLD
+            change_quality(item, +1)
           end
         end
       end
@@ -59,6 +59,14 @@ class GildedRose
   end
 
   private
+
+  def is_normal_item?(item)
+    !is_aged_brie?(item) && !is_backstage_pass?(item) && !is_backstage_pass?(item) && !is_sulfuras?(item)
+  end
+
+  def change_quality(item, amount)
+    item.quality = item.quality + amount
+  end
 
   def is_aged_brie?(item)
     item.name == "Aged Brie"
@@ -70,5 +78,9 @@ class GildedRose
 
   def is_sulfuras?(item)
     item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def is_conjured?(item)
+    item.name == "Conjured Mana Cake"
   end
 end
