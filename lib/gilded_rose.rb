9491924ec0a89f06e67353d2dc1e82c45b52b1ea
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-07 16:14:13 +0000

    Refactor Item into own class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,3 +1,5 @@
+require_relative 'item'
+
 class GildedRose
 
   def initialize(items)
@@ -50,19 +52,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
