730776a42046a11fa303b7b53d83d7aecd86962e
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-08 14:33:41 +0000

    Further refactor method names

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -26,21 +26,6 @@ class GildedRose
     item.quality = MIN_THRESHOLD
   end
 
-  def calculate_backstage_quality(item)
-    change_quality(item, 1) if item.sell_in > BSPASS_TEN_DAY_THRESHOLD
-    change_quality(item, 2) if item.sell_in < BSPASS_TEN_DAY_THRESHOLD && item.sell_in > BSPASS_FIVE_DAY_THRESHOLD
-    change_quality(item, 3) if item.sell_in < BSPASS_FIVE_DAY_THRESHOLD
-    no_quality(item) if passed_sellby_date?(item)
-  end
-
-  def calculate_conjured_quality(item)
-    passed_sellby_date?(item) ? (change_quality(item, -4)) : (change_quality(item, -2))
-  end
-
-  def calculate_brie_quality(item)
-    change_quality(item, +1)
-  end
-
   def calculate_normal_quality(item)
     passed_sellby_date?(item) ? (change_quality(item, -2)) : (change_quality(item, -1))
   end
@@ -50,14 +35,14 @@ class GildedRose
   end
 
   def change_quality(item, amount)
-    item.quality += amount if is_in_range?(item) || (is_aged_brie?(item) && item.quality < MAX_THRESHOLD)
+    item.quality += amount if quality_is_in_range?(item) || (is_aged_brie?(item) && item.quality < MAX_THRESHOLD)
   end
 
   def change_sellin(item)
     item.sell_in -= 1 unless is_sulfuras?(item)
   end
 
-  def is_in_range?(item)
+  def quality_is_in_range?(item)
     item.quality > MIN_THRESHOLD && item.quality < MAX_THRESHOLD
   end
 
@@ -69,8 +54,19 @@ class GildedRose
     item.name == "Aged Brie"
   end
 
+  def calculate_brie_quality(item)
+    change_quality(item, +1)
+  end
+
   def is_backstage_pass?(item)
     item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def calculate_backstage_quality(item)
+    change_quality(item, 1) if item.sell_in > BSPASS_TEN_DAY_THRESHOLD
+    change_quality(item, 2) if item.sell_in < BSPASS_TEN_DAY_THRESHOLD && item.sell_in > BSPASS_FIVE_DAY_THRESHOLD
+    change_quality(item, 3) if item.sell_in < BSPASS_FIVE_DAY_THRESHOLD
+    no_quality(item) if passed_sellby_date?(item)
   end
 
   def is_sulfuras?(item)
@@ -79,5 +75,9 @@ class GildedRose
 
   def is_conjured?(item)
     item.name == "Conjured Mana Cake"
+  end
+
+  def calculate_conjured_quality(item)
+    passed_sellby_date?(item) ? (change_quality(item, -4)) : (change_quality(item, -2))
   end
 end
